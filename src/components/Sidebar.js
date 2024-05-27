304d356ac6389724a21720636b007b8a3c4781a8
Abhishek Mishra <ahm.rimer@gmail.com>
2024-05-27 12:17:58 +0530

    Refactored Sidebar component to manage chats instead of folders.

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,57 +1,18 @@
-import React, { useState, useContext } from 'react';
-import { FolderContext } from '../context/FolderContext';
+import React, { useState } from 'react';
 
 function Sidebar() {
-  // Feature: Selecting a Folder
-  const { folders, setFolders, selectedFolder, setSelectedFolder } = useContext(FolderContext);
-  const [newFolder, setNewFolder] = useState('');
-  const [renamingFolder, setRenamingFolder] = useState(null);
+  const [chats, setChats] = useState([]);
+  const [renamingChat, setRenamingChat] = useState(null);
   const [renameValue, setRenameValue] = useState('');
-
-  // Feature: Adding a New Folder
-  const addFolder = () => {
-    if (newFolder) {
-      setFolders([...folders, { name: newFolder, chats: [] }]);
-      setNewFolder('');
-    }
-  };
-
-  // Feature: Renaming an Existing Folder
-  const renameFolder = (index) => {
-    const updatedFolders = [...folders];
-    updatedFolders[index].name = renameValue;
-    setFolders(updatedFolders);
-    setRenamingFolder(null);
-    setRenameValue('');
-  };
-
-  // Feature: Deleting a Folder
-  const deleteFolder = (index) => {
-    const updatedFolders = folders.filter((_, i) => i !== index);
-    setFolders(updatedFolders);
-    if (selectedFolder === folders[index].name) {
-      setSelectedFolder('Default');
-    }
-  };
 
   return (
     <div className="col-3 bg-light border-right vh-100 p-3">
       <div>
-        <input
-          type="text"
-          className="form-control mb-2"
-          placeholder="New Folder"
-          value={newFolder}
-          onChange={(e) => setNewFolder(e.target.value)}
-        />
-        <button className="btn btn-primary btn-block mb-3" onClick={addFolder}>
-          Add Folder
-        </button>
       </div>
       <ul className="list-group list-group-flush">
-        {folders.map((folder, index) => (
+        {chats.map((chat, index) => (
           <li key={index} className="list-group-item d-flex justify-content-between align-items-center">
-            {renamingFolder === index ? (
+            {renamingChat === index ? (
               <>
                 <input
                   type="text"
@@ -65,12 +26,15 @@ function Sidebar() {
               </>
             ) : (
               <>
-                <span onClick={() => setSelectedFolder(folder.name)}>{folder.name}</span>
+                <span>{chat.name}</span>
                 <div>
-                  <button className="btn btn-sm btn-warning mr-2" onClick={() => setRenamingFolder(index)}>
+                  <button className="btn btn-sm btn-warning mr-2" onClick={() => setRenamingChat(index)}>
                     Rename
                   </button>
-                  <button className="btn btn-sm btn-danger" onClick={() => deleteFolder(index)}>
+                  <button className="btn btn-sm btn-danger" onClick={() => {
+                    const updatedChats = chats.filter((_, i) => i !== index);
+                    setChats(updatedChats);
+                  }}>
                     Delete
                   </button>
                 </div>
